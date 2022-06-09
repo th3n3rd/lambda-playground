70685ee0dce68231cf8101cbcd20e1a001e8ed80
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-06-09 16:33:56 +0100

    First commit

---
+++ scripts/acceptance.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+cd "$SCRIPT_DIR/.." || exit
+
+
+cleanup() {
+    kill $LAMBDA_API_MOCK_PID
+}
+
+echo "Deploying locally"
+sam local start-api &
+LAMBDA_API_MOCK_PID=$!
+trap cleanup EXIT
+sleep 3
+echo "Testing sample lambda function firing an api request"
+API_TRIGGERED_EXPECTED='{"message":"hello world"}'
+API_TRIGGERED_ACTUAL=$(curl --silent "http://127.0.0.1:3000/hello")
+if [ "$API_TRIGGERED_ACTUAL" != "$API_TRIGGERED_EXPECTED" ]; then
+    echo "Api acceptance test failed." >&2
+    echo "expected: $API_TRIGGERED_EXPECTED" >&2
+    echo "actual: $API_TRIGGERED_ACTUAL" >&2
+    exit 1
+fi
+
+echo "Testing sample lambda function firing an event"
+EVENT_TRIGGERED_EXPECTED='{"statusCode":200,"body":"{\"message\":\"hello world\"}"}'
+EVENT_TRIGGERED_ACTUAL=$(sam local invoke "HelloWorldFunction" -e events/event.json)
+if [ "$EVENT_TRIGGERED_ACTUAL" != "$EVENT_TRIGGERED_EXPECTED" ]; then
+    echo "Event acceptance test failed." >&2
+    echo "expected: $EVENT_TRIGGERED_EXPECTED" >&2
+    echo "actual: $EVENT_TRIGGERED_ACTUAL" >&2
+    exit 1
+fi
+
+echo "Acceptance test passed"
+
+
