6d806026666f7e1948db566530bf99f7f0cf98f3
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-06-13 17:28:06 +0100

    Allow local deployment to localstack

--- scripts/acceptance.sh
+++ scripts/acceptance.sh
@@ -7,34 +7,30 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 cd "$SCRIPT_DIR/.." || exit
 
 cleanup() {
-    kill $LAMBDA_API_MOCK_PID
+    # Because of a bug in samlocal (localstack), the aws stack cannot be deleted hence no idempotency
+    # Details: https://github.com/localstack/localstack/issues/5949
+    localstack stop
 }
 
-echo "Building locally"
-sam build --beta-features
-
-echo "Deploying locally"
-sam local start-api &
-LAMBDA_API_MOCK_PID=$!
+localstack start -d
 trap cleanup EXIT
-sleep 3
+
+echo "Building locally"
+samlocal build --beta-features
+
+echo "Deploy locally on localstack (as Zip onto S3)"
+samlocal deploy --resolve-s3
+
+LAMBDA_API_ID=$(awslocal apigateway get-rest-apis --region eu-west-1 | jq -r '.items[0].id')
+LAMBDA_API_URL="https://$LAMBDA_API_ID.execute-api.localhost.localstack.cloud:4566/Prod/hello"
+
 echo "Testing sample lambda function firing an api request"
 API_TRIGGERED_EXPECTED='{"message":"hello world"}'
-API_TRIGGERED_ACTUAL=$(curl --silent "http://127.0.0.1:3000/hello")
+API_TRIGGERED_ACTUAL=$(curl --silent "$LAMBDA_API_URL")
 if [ "$API_TRIGGERED_ACTUAL" != "$API_TRIGGERED_EXPECTED" ]; then
     echo "Api acceptance test failed." >&2
     echo "expected: $API_TRIGGERED_EXPECTED" >&2
     echo "actual: $API_TRIGGERED_ACTUAL" >&2
-    exit 1
-fi
-
-echo "Testing sample lambda function firing an event"
-EVENT_TRIGGERED_EXPECTED='{"statusCode":200,"body":"{\"message\":\"hello world\"}"}'
-EVENT_TRIGGERED_ACTUAL=$(sam local invoke "HelloWorldFunction" -e events/event.json)
-if [ "$EVENT_TRIGGERED_ACTUAL" != "$EVENT_TRIGGERED_EXPECTED" ]; then
-    echo "Event acceptance test failed." >&2
-    echo "expected: $EVENT_TRIGGERED_EXPECTED" >&2
-    echo "actual: $EVENT_TRIGGERED_ACTUAL" >&2
     exit 1
 fi
 
