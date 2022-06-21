14bc5758cde4cdfa5b7db2e185abcebc4350d702
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-06-21 10:27:07 +0100

    Fix test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 cd "$SCRIPT_DIR/.." || exit
 
-functions=(hello-world receptionist last-received-person)
+functions=(receptionist last-received-person)
 
 for fn in "${functions[@]}"; do
     pushd "$fn"
